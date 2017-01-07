6f1df86dce701773f957fc54ba2b21e7c9c40064
J Feustel <jfeustel@gmail.com>
2017-01-06 20:52:18 -0600

    Completed
    Very exciting. It counts vowels.

--- VowelsConsole/Program.cs
+++ VowelsConsole/Program.cs
@@ -9,9 +9,23 @@ namespace VowelsConsole
     class Program
     {
         static void Main(string[] args)            
-        {
-            Console.WriteLine("Hello world!");
-            Console.ReadLine();
+        {           
+            int count = 0;
+            Console.WriteLine("Welcome to Vowel Counter 2017!");
+            Console.WriteLine("Enter text:");
+            String txt = Console.ReadLine();
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (txt[i] == 'a' ||
+                    txt[i] == 'e' ||
+                    txt[i] == 'i' ||
+                    txt[i] == 'o' ||
+                    txt[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("The vowel count is " + count);
         }
     }
 }
